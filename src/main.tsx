5a10e1194a9e0104231fb15720ca8aebe5a4a702
xuniquepatel <patelunique1@gmail.com>
2025-08-31 00:43:35 +0530

    updateX

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-// src/main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 
-const el = document.getElementById("root");
-if (!el) throw new Error("#root not found");
-
-createRoot(el).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
